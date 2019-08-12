6f179de89723081d990d1359981010f077678623
Jonathan Lennox <jonathan.lennox@8x8.com>
2019-08-12 11:14:05 -0400

    Don't use deprecated bouncycastle AESFastEngine API.

--- src/test/java/org/jitsi/srtp/SRTPCipherCTRTest.java
+++ src/test/java/org/jitsi/srtp/SRTPCipherCTRTest.java
@@ -3,7 +3,7 @@ package org.jitsi.srtp;
 import static org.junit.Assert.*;
 import java.util.Arrays;
 import javax.xml.bind.DatatypeConverter;
-import org.bouncycastle.crypto.engines.AESFastEngine;
+import org.bouncycastle.crypto.engines.AESEngine;
 //import org.jitsi.util.OSUtils;
 import org.junit.Test;
 
@@ -32,7 +32,7 @@ public class SRTPCipherCTRTest
     @Test
     public void testJavaCTRAES()
     {
-        SRTPCipherCTR cipher = new SRTPCipherCTRJava(new AESFastEngine());
+        SRTPCipherCTR cipher = new SRTPCipherCTRJava(new AESEngine());
         cipher.init(TV_Key);
         byte[] data = new byte[TV_Cipher_AES_1.length];
 
