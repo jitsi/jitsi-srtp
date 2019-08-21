d9763880e2e322dc23473d2b14679f75ba351792
Jonathan Lennox <jonathan.lennox@8x8.com>
2019-08-21 16:15:02 -0400

    Add internal code to read necessary fields of SRTP and SRTCP packets.
    Remove dependency on jitsi-rtp.

--- src/main/java/org/jitsi/srtp/SRTCPCryptoContext.java
+++ src/main/java/org/jitsi/srtp/SRTCPCryptoContext.java
@@ -19,8 +19,6 @@ import java.util.*;
 
 import org.bouncycastle.crypto.params.*;
 import org.jitsi.bccontrib.params.*;
-import org.jitsi.rtp.rtcp.RtcpHeader;
-import org.jitsi.rtp.util.SrtpUtils;
 import org.jitsi.utils.*;
 
 /**
@@ -213,7 +211,7 @@ public class SRTCPCryptoContext
      */
     public void processPacketAESCM(ByteArrayBuffer pkt, int index)
     {
-        int ssrc = (int) RtcpHeader.Companion.getSenderSsrc(pkt.getBuffer(), pkt.getOffset());
+        int ssrc = SRTCPPacket.getSenderSsrc(pkt);
 
         /* Compute the CM IV (refer to chapter 4.1.1 in RFC 3711):
         *
@@ -310,11 +308,11 @@ public class SRTCPCryptoContext
         boolean decrypt = false;
         int tagLength = policy.getAuthTagLength();
 
-        if (pkt.getLength() < 8 /* RTCP header and ssrc */ + 4 /* index */ + tagLength)
+        if (!SRTCPPacket.validatePacket(pkt, tagLength))
             /* Too short to be a valid SRTCP packet */
             return false;
 
-        int indexEflag = SrtpUtils.Companion.getSrtcpIndex(pkt, tagLength);
+        int indexEflag = SRTCPPacket.getIndex(pkt, tagLength);
 
         if ((indexEflag & 0x80000000) == 0x80000000)
             decrypt = true;
