408c15bbbbf9a64db9d560ad4cd356d8b45cc1c1
Jonathan Lennox <jonathan.lennox@8x8.com>
2020-08-27 16:11:22 -0400

    Test decrypt-only mode.

--- src/test/java/org/jitsi/srtp/SrtpValidationTest.java
+++ src/test/java/org/jitsi/srtp/SrtpValidationTest.java
@@ -196,6 +196,50 @@ public class SrtpValidationTest {
         }
     }
 
+    @Test
+    public void skipDecryptionCtrHmac() throws Exception
+    {
+        SrtpPolicy policy =
+            new SrtpPolicy(SrtpPolicy.AESCM_ENCRYPTION, 128/8,
+                SrtpPolicy.HMACSHA1_AUTHENTICATION, 160/8,
+                80/8, 112/8 );
+        Logger logger = new LoggerImpl(getClass().getName());
+
+        for (int len = srtp_ciphertext.length; len > 0; len--)
+        {
+            SrtpContextFactory receiverFactory = new SrtpContextFactory(false, test_key, test_key_salt, policy, policy, logger);
+            SrtpCryptoContext rtpRecv = receiverFactory.deriveContext(0xcafebabe, 0);
+
+            ByteArrayBuffer rtpPkt = new ByteArrayBufferImpl(Arrays.copyOf(srtp_ciphertext, len), 0, len);
+
+            SrtpErrorStatus status = rtpRecv.reverseTransformPacket(rtpPkt, true);
+
+            if (len == srtp_ciphertext.length)
+            {
+                assertEquals(SrtpErrorStatus.OK, status, "Rejected valid SRTP packet when skipping decryption");
+            }
+            else
+            {
+                assertNotEquals(SrtpErrorStatus.OK, status, "Accepted truncated SRTP packet when skipping decryption");
+            }
+        }
+
+        for (int i = 0; i < srtp_ciphertext.length; i++) {
+            for (int j = 0; j < 8; j++) {
+                SrtpContextFactory receiverFactory = new SrtpContextFactory(false, test_key, test_key_salt, policy, policy, logger);
+                SrtpCryptoContext rtpRecv = receiverFactory.deriveContext(0xcafebabe, 0);
+
+                ByteArrayBuffer rtpPkt = new ByteArrayBufferImpl(srtp_ciphertext.clone(), 0, srtp_ciphertext.length);
+
+                /* Flip one bit */
+                rtpPkt.getBuffer()[i] ^= (1 << j);
+
+                assertNotEquals(SrtpErrorStatus.OK, rtpRecv.reverseTransformPacket(rtpPkt, true),
+                    "Accepted RTP packet with bit flipped when skipping decryption");
+            }
+        }
+    }
+
     /* GCM test cases from libsrtp's srtp_driver.c. */
     private static final byte[] test_key_gcm =
         parseHexBinary("0001020304050607" +
@@ -366,6 +410,51 @@ public class SrtpValidationTest {
 
                 assertNotEquals(SrtpErrorStatus.OK, rtcpRecv.reverseTransformPacket(rtcpPkt),
                     "Accepted RTCP packet with bit flipped");
+            }
+        }
+    }
+
+    @Test
+    public void skipDecryptionGcm() throws Exception
+    {
+        Logger logger = new LoggerImpl(getClass().getName());
+
+        SrtpPolicy policy =
+            new SrtpPolicy(SrtpPolicy.AESGCM_ENCRYPTION, 128/8,
+                SrtpPolicy.NULL_AUTHENTICATION, 0,
+                128/8, 96/8 );
+
+        for (int len = srtp_ciphertext_gcm.length; len > 0; len--)
+        {
+            SrtpContextFactory receiverFactory = new SrtpContextFactory(false, test_key_gcm, test_key_salt_gcm, policy, policy, logger);
+            SrtpCryptoContext rtpRecv = receiverFactory.deriveContext(0xcafebabe, 0);
+
+            ByteArrayBuffer rtpPkt = new ByteArrayBufferImpl(Arrays.copyOf(srtp_ciphertext_gcm, len), 0, len);
+
+            SrtpErrorStatus status = rtpRecv.reverseTransformPacket(rtpPkt, true);
+
+            if (len == srtp_ciphertext_gcm.length)
+            {
+                assertEquals(SrtpErrorStatus.OK, status, "Rejected valid SRTP packet when skipping decryption");
+            }
+            else
+            {
+                assertNotEquals(SrtpErrorStatus.OK, status, "Accepted truncated SRTP packet when skipping decryption");
+            }
+        }
+
+        for (int i = 0; i < srtp_ciphertext_gcm.length; i++) {
+            for (int j = 0; j < 8; j++) {
+                SrtpContextFactory receiverFactory = new SrtpContextFactory(false, test_key_gcm, test_key_salt_gcm, policy, policy, logger);
+                SrtpCryptoContext rtpRecv = receiverFactory.deriveContext(0xcafebabe, 0);
+
+                ByteArrayBuffer rtpPkt = new ByteArrayBufferImpl(srtp_ciphertext_gcm.clone(), 0, srtp_ciphertext_gcm.length);
+
+                /* Flip one bit */
+                rtpPkt.getBuffer()[i] ^= (1 << j);
+
+                assertNotEquals(SrtpErrorStatus.OK, rtpRecv.reverseTransformPacket(rtpPkt, true),
+                    "Accepted RTP packet with bit flipped when skipping decryption");
             }
         }
     }
