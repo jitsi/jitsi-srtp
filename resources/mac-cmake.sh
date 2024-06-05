06ec3e170ec2c7892a5a065c48768907b52aef9b
Jonathan Lennox <jonathan.lennox@8x8.com>
2024-06-05 15:31:43 -0400

    Fix: run macOS GitHub actions on appropriate worker arches. (#55)

--- resources/mac-cmake.sh
+++ resources/mac-cmake.sh
@@ -15,17 +15,22 @@ case $ARCH in
     "x86-64"|"x86_64")
         INSTALL_PREFIX_ARCH=x86-64
         OSX_ARCH=x86_64
+        HOMEBREW_ROOT=/usr/local
         ;;
     "arm64"|"aarch64")
         INSTALL_PREFIX_ARCH=aarch64
         OSX_ARCH=arm64
+        HOMEBREW_ROOT=/opt/homebrew
         ;;
 esac
+
+# For reasons that are not clear, this needs to be an environment
+# variable rather than a cmake command-line define
+export OPENSSL_ROOT_DIR="$HOMEBREW_ROOT/opt/openssl@$OPENSSL_VERSION"
 
 cmake -B cmake-build \
     -DJITSI_SRTP_LIBSSL_VERSION="$OPENSSL_VERSION" \
     -DJAVA_HOME="$JAVA_HOME" \
     -DCMAKE_INSTALL_PREFIX="src/main/resources/darwin-$INSTALL_PREFIX_ARCH" \
-    -DCMAKE_OSX_ARCHITECTURES="$OSX_ARCH" \
-    -DOPENSSL_ROOT_DIR="/usr/local/opt/openssl@$OPENSSL_VERSION"
+    -DCMAKE_OSX_ARCHITECTURES="$OSX_ARCH"
 cmake --build cmake-build --config Release --target install
